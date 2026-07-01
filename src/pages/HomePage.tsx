5a66c3e1e220762003c38497d37c6917627a6800
1nn4a <obengpthomas@gmail.com>
2026-07-01 13:56:06 +0100

    logged out

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -146,20 +146,7 @@ We enable specialist businesses and independent professionals to connect, share 
 
 <div className="relative mt-10">
  <div
-  className="hidden md:block absolute pointer-events-none -z-10"
-  style={{
-    backgroundImage: `url(${cardsBg})`,
-    width: '1197px',
-    height: '1216px',
-    top: 0,
-    left: '50%',
-    transform: 'translateX(-50%)',
-    backgroundSize: '1197px 1216px',
-    backgroundRepeat: 'no-repeat',
-  }}
-/>
-<div
-  className="md:hidden absolute inset-0 opacity-100 pointer-events-none -z-10"
+  className="absolute top-0 left-1/2 -translate-x-1/2 w-screen h-full opacity-100 pointer-events-none -z-10"
   style={{
     backgroundImage: `url(${cardsBg})`,
     backgroundSize: '100% 100%',
